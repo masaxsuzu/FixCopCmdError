c86ebc4f40ba6f15f4a74eecbba9e4134fd2050b
masaxsuzu <masaxsuzu+github@gmail.com>
2021-06-19 23:54:16 +0900

    Fix warnings and an error of FxCop

--- Build.ps1
+++ Build.ps1
@@ -1,10 +1,9 @@
 $msbuild2019 = 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe'
 $msbuild2022 = 'C:\Program Files\Microsoft Visual Studio\2022\Preview\MSBuild\Current\Bin\MSBuild.exe'
 
-# Post-build Code Analysis (FxCopCmd.exe) has been deprecated in favor of FxCop analyzers
 
-# VS2019 with warnings
-& $msbuild2019 .\netfx48\netfx48.csproj /t:ReBuild /p:RunCodeAnalysis=true
+# Build with no error
+# https://docs.microsoft.com/ja-jp/visualstudio/code-quality/migrate-from-fxcop-analyzers-to-net-analyzers?view=vs-2022
 
-# VS2022 with an error: FxCopCmd.exe is no longer available.
-& $msbuild2022 .\netfx48\netfx48.csproj /t:ReBuild /p:RunCodeAnalysis=true
+& $msbuild2019 .\netfx48\netfx48.csproj /t:ReBuild /p:AnalysisMode=AllEnabledByDefault
+& $msbuild2022 .\netfx48\netfx48.csproj /t:ReBuild /p:AnalysisMode=AllEnabledByDefault
